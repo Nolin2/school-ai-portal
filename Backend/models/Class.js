86d883e91474f0375a10c69a31ff76fc1773b005
Nolin Masai <n89820457@gmail.com>
2025-11-14 18:00:12 +0300

    Create Class.js

---
+++ Backend/models/Class.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const classSchema = new mongoose.Schema({
+  name: { type: String, required:true },
+  subjects: [{ type: mongoose.Schema.Types.ObjectId, ref:'Subject' }]
+});
+
+export default mongoose.model('Class', classSchema);
